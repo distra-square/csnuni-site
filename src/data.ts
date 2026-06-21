5ee8e109c174fd1cdd333d2abb2a2e9a2e3c97dd
distra-square <andreaiodice8@gmail.com>
2026-06-21 23:50:59 +0200

    Update data.ts
    agg biostudenti

--- src/data.ts
+++ src/data.ts
@@ -495,8 +495,12 @@ export const UNIVERSITY_AREAS: UniversityArea[] = [
         description: 'Area Biologia.', 
         link: 'https://instagram.com/biostudenti_unina', 
         instagram: 'https://instagram.com/biostudenti_unina',
-        telegram: 'https://t.me/matricole_biologia_unina', 
-        logo: '/BIOSTUDENTI.png' 
+        customLinks: [
+          { 
+            label: 'Gruppo Matricole', url: 'https://t.me/matricole_biologia_unina', type: 'telegram' 
+          }
+        ]        
+          logo: '/BIOSTUDENTI.png' 
       },
       { 
         id: 'asgunina', 
